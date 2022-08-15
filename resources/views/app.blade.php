23366ad543e492fab5c17efad27397f7586a53bf
Praeveen Ganesharaja <praeveen099@gmail.com>
2022-08-15 11:59:45 +0100

    Added views for the posts and users

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
 
         <!-- Bootstrap CSS -->
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="//cdn.datatables.net/1.10.7/css/jquery.dataTables.min.css">
+        <link rel="stylesheet" href="//cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
 
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -25,14 +25,12 @@
     <body>
         <div class="container">
             @yield('content')
-
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nobis, id in necessitatibus amet dignissimos blanditiis dolorum numquam ratione neque quia laborum doloribus. Odit non, delectus voluptatem reiciendis atque repellat eaque!
         </div>
 
         <!-- jQuery -->
         <script src="//code.jquery.com/jquery.js"></script>
         <!-- DataTables -->
-        <script src="//cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
+        <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
         <!-- Bootstrap JavaScript -->
         <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
         <!-- App scripts -->
