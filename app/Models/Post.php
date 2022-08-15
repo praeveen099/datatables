205013276897e301446c4e4d484fe3f174463a18
Praeveen Ganesharaja <praeveen099@gmail.com>
2022-08-15 11:50:24 +0100

    Added a table name variable

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,11 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\DataTables\PostsDataTable;
+
 
 class Post extends Model
 {
     use HasFactory;
 
+    protected $table = "posts";
     // protected $fillable = [];
 
     protected $guarded = [];
